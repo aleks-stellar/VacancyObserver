10865ae19a9cdfbbb87424a3f21cf6d544f47146
Aksenov Aleksandr <sandroaksyonov@gmail.com>
2025-09-29 16:10:12 +0300

    Create HeadHunterAPI class

---
+++ src/api/hh_api.py
@@ -0,0 +1,13 @@
+from src.api.base_api import BaseAPI
+
+
+class HeadHunterAPI(BaseAPI):
+
+    def __init__(self):
+        pass
+
+    def send_request(self):
+        pass
+
+    def process_response(self):
+        pass
