88adcc291ee3ba60a240746f9e59603af4a2abaf
Aksenov Aleksandr <sandroaksyonov@gmail.com>
2025-09-29 16:17:38 +0300

    Small fixes

--- src/api/base_api.py
+++ src/api/base_api.py
@@ -2,12 +2,14 @@ from abc import ABC, abstractmethod
 
 
 class BaseAPI(ABC):
-    """Абстрактный класс для работы с API сервисов с вакансиями."""
+    """Абстрактный класс для работы с API сервисов с вакансиями"""
 
     @abstractmethod
     def send_request(self):
+        """Метод отправки POST-запроса на сервер API"""
         pass
 
     @abstractmethod
     def process_response(self):
+        """Метод обработки GET-ответа"""
         pass
--- src/api/hh_api.py
+++ src/api/hh_api.py
@@ -2,6 +2,7 @@ from src.api.base_api import BaseAPI
 
 
 class HeadHunterAPI(BaseAPI):
+    """Класс для работы с API сервиса HeadHunter"""
 
     def __init__(self):
         pass
