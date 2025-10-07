35d8d54b2ee8ba37203f439e4560eba5a1143664
Aksenov Aleksandr <sandroaksyonov@gmail.com>
2025-10-08 00:13:08 +0300

    Fix the first test

---
+++ tests/api/test_hh_api.py
@@ -0,0 +1,17 @@
+from unittest.mock import patch
+from src.api.hh_api import HeadHunterAPI
+
+
+@patch("src.api.hh_api.requests.get")
+def test_hh_api_object_init(mock_get, vacancies_list):
+    """Тестируем работу метода get_vacancies"""
+
+    mock_get.return_value.json.return_value = vacancies_list
+
+    hh_api = HeadHunterAPI()
+    vacancies = hh_api.get_vacancies()
+
+    mock_get.assert_called_once()
+
+    assert vacancies["items"][0]["id"] == "93353083"
+    assert len(vacancies["items"]) == 2
