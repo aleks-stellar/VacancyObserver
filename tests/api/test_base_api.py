d521d148adef8055f6ef7dc4c3228d9292c9c754
Aksenov Aleksandr <sandroaksyonov@gmail.com>
2025-10-08 00:43:29 +0300

    Create tests for check abstract class

---
+++ tests/api/test_base_api.py
@@ -0,0 +1,29 @@
+import pytest
+from abc import ABC
+from src.api.base_api import BaseAPI
+from src.api.hh_api import HeadHunterAPI
+
+
+def test_base_api_is_abstract():
+    """Проверяем, что класс BaseAPI является абстрактным"""
+    assert issubclass(BaseAPI, ABC)
+
+
+def test_base_api_has_abstract_methods():
+    """Проверяем, что у BaseAPI есть абстрактные методы"""
+    abstract_methods = BaseAPI.__abstractmethods__
+    assert "_send_request" in abstract_methods
+    assert "_process_response" in abstract_methods
+    assert "get_vacancies" in abstract_methods
+
+
+def test_base_api_cannot_be_instantiated():
+    """Проверяем, что нельзя создать экземпляр BaseAPI"""
+    with pytest.raises(TypeError):
+        BaseAPI()
+
+
+def test_hh_api_can_be_instantiated():
+    """Проверяем, что HeadHunterAPI реализует абстрактные методы и может быть создан"""
+    hh_api = HeadHunterAPI()
+    assert hh_api is not None
