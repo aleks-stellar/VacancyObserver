4961160d915adf2d6edcf20ceeb865859a1f5653
Aksenov Aleksandr <sandroaksyonov@gmail.com>
2025-10-19 04:07:33 +0300

    Add test

--- tests/connector/test_json_worker.py
+++ tests/connector/test_json_worker.py
@@ -19,3 +19,24 @@ def test_add_vacancy_data(tmp_path, vacancy_data1: Dict[str, str | Dict]) -> Non
     assert isinstance(data, list)
     assert len(data) == 1
     assert data[0]["name"] == "Python Developer"
+
+
+def test_add_two_vacancies_data(
+        tmp_path,
+        vacancy_data1: Dict[str, str | Dict],
+        vacancy_data2: Dict[str, str | Dict]
+) -> None:
+    """Проверяем работу метода add_vacancy_data при добавлении двух вакансий"""
+    tmp_file_name = "test.json"
+    worker = JSONWorker(path=tmp_path, file_name=tmp_file_name)
+
+    worker.add_vacancy_data(vacancy_data1)
+    worker.add_vacancy_data(vacancy_data2)
+
+    with open(worker.full_path, "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+    assert isinstance(data, list)
+    assert len(data) == 2
+    assert data[0]["name"] == "Python Developer"
+    assert data[1]["name"] == "Web Developer"
