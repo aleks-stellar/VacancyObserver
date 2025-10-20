f2a89def898944be03166927fa5a1e601c5831ed
Aksenov Aleksandr <sandroaksyonov@gmail.com>
2025-10-20 14:43:59 +0300

    Fix fixtures

--- tests/connector/conftest.py
+++ tests/connector/conftest.py
@@ -8,7 +8,7 @@ def vacancy_data1() -> Dict[str, str | Dict]:
     """Фикстура с данными о вакансии"""
     return {
         "name": "Python Developer",
-        "alternate_url": "<https://hh.ru/vacancy/123456>",
+        "alternate_url": "https://hh.ru/vacancy/123456",
         "salary": {"from": 350000, "to": 450000, "currency": "RUB", "gross": False},
         "requirement": "Требования: опыт работы от 3 лет..."
     }
@@ -19,7 +19,7 @@ def vacancy_data2() -> Dict[str, str | Dict]:
     """Фикстура с данными о вакансии"""
     return {
         "name": "Web Developer",
-        "alternate_url": "<https://hh.ru/vacancy/123457>",
+        "alternate_url": "https://hh.ru/vacancy/123457",
         "salary": {"from": 250000, "to": 400000, "currency": "RUB", "gross": False},
         "requirement": "Требования: опыт работы от 5 лет..."
     }
