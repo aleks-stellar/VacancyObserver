0acb9e56e64b715aa6b207942687d8f18f772387
Aksenov Aleksandr <sandroaksyonov@gmail.com>
2025-10-20 11:47:38 +0300

    Add tests for init

--- tests/connector/test_json_worker.py
+++ tests/connector/test_json_worker.py
@@ -1,7 +1,33 @@
 import json
+from pathlib import WindowsPath
 from typing import Dict
 
 from src.connector.json_worker import JSONWorker
+
+
+def test_init_and_validation() -> None:
+    """Тестируем инициализацию объектов класса JSONWorker а также валидацию пути и имени внутри инициализации"""
+    worker = JSONWorker()
+    assert worker.full_path == WindowsPath("D:/Science/Python/VacancyObserver/data/vacancies.json")
+
+    worker = JSONWorker(file_name="vacancies_data")
+    assert worker.full_path == WindowsPath("D:/Science/Python/VacancyObserver/data/vacancies_data.json")
+
+    worker = JSONWorker(path="D:/Science/Python/VacancyObserver")
+    assert worker.full_path == WindowsPath("D:/Science/Python/VacancyObserver/vacancies.json")
+
+    worker = JSONWorker(path="D:/Science/Python/VacancyObserver", file_name="data.json")
+    assert worker.full_path == WindowsPath("D:/Science/Python/VacancyObserver/data.json")
+
+    worker = JSONWorker(path="D:/Python/Vacancy", file_name="data.json")
+    assert worker.full_path == WindowsPath("D:/Python/Vacancy/data.json")
+
+
+def test_class_attributes() -> None:
+    """Проверяем атрибуты класса"""
+    worker = JSONWorker()
+    assert worker.DEFAULT_NAME == "vacancies.json"
+    assert worker.DEFAULT_PATH == WindowsPath("D:/Science/Python/VacancyObserver/data")
 
 
 def test_add_vacancy_data(tmp_path, vacancy_data1: Dict[str, str | Dict]) -> None:
