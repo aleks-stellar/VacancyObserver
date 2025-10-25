64854f83f738008735907b9ca776771355e0356d
Aksenov Aleksandr <sandroaksyonov@gmail.com>
2025-10-25 08:21:16 +0300

    Add tests for input and output

--- src/user_interface/interface.py
+++ src/user_interface/interface.py
@@ -1,9 +1,11 @@
+from src.connector.json_worker import JSONWorker
+from src.vacancy.base_vacancy import BaseVacancy
+
+
 def user_interface() -> None:
     """Функция для взаимодействия пользователя с приложением"""
-    first_str = ('Введите "1", если хотите получить все вакансии по ключевому слову и "2", '
-                    'если хотите получить топ N вакансий по минимальной зарплате')
-
-    print(first_str)
+    print(('Введите "1", если хотите получить все вакансии по ключевому слову и "2", '
+                    'если хотите получить топ N вакансий по минимальной зарплате'))
 
     try:
         operation_choice = int(input("Введите число: "))
@@ -14,9 +16,13 @@ def user_interface() -> None:
         print(f"Вы выбрали получить все вакансии по ключевому слову. "
               "Введите ключевое слово: ", end="")
 
-        user_keyword = input()
-        string_user_keyword = f'Вы ввели слово "{user_keyword}"'
-        print(string_user_keyword)
+        user_keyword = input().lower()
+        print(f'Вы ввели слово "{user_keyword}"')
+
+        print(f'Вакансии по ключевому слову '
+                               f'"{user_keyword}" сохранены по пути "../data/{user_keyword}.json"')
+
+        print(f"Хотите получить топ N вакансий по минимальной зарплате? да/нет: ", end="")
 
     if operation_choice == 2:
         print(f"Вы выбрали получить топ N вакансий по минимальной зарплате. "
@@ -24,8 +30,7 @@ def user_interface() -> None:
 
         try:
             user_min_salary = int(input())
-            string_min_salary = f"Вы ввели минимальную зарплату {user_min_salary}"
-            print(string_min_salary)
+            print(f"Вы ввели минимальную зарплату {user_min_salary}")
         except ValueError:
             raise ValueError('Необходимо ввести целое число')
 
@@ -33,10 +38,12 @@ def user_interface() -> None:
 
         try:
             user_n_for_top = int(input())
-            string_n_for_top = f"Вы ввели число N = {user_n_for_top}"
-            print(string_n_for_top)
+            print(f"Вы ввели число N = {user_n_for_top}")
         except ValueError:
             raise ValueError('Необходимо ввести целое число')
+
+        print((f'Топ {user_n_for_top} вакансий по минимальной зарплате {user_min_salary} руб. '
+                               f'сохранены по пути "../data/top_{user_n_for_top}_salary_{user_min_salary}.json"'))
 
 
 if __name__ == '__main__':
--- tests/user_interface/test_interface.py
+++ tests/user_interface/test_interface.py
@@ -6,17 +6,22 @@ from src.user_interface.interface import user_interface
 
 def test_user_interface_output(capsys, first_string) -> None:
     """Тестируем поток вывода"""
-    with patch("builtins.input", side_effect=["1", "Python"]):
+    with patch("builtins.input", side_effect=["1", "Python", "нет"]):
         user_interface()
 
         captured = capsys.readouterr()
         string_keyword = ("Вы выбрали получить все вакансии по ключевому слову. "
                                  "Введите ключевое слово: ")
-        user_keyword_str = f'Вы ввели слово "Python"'
+        user_keyword_str = f'Вы ввели слово "python"'
+        string_for_save_vacancies = f'Вакансии по ключевому слову "python" сохранены по пути "../data/python.json"'
+
+        next_step_string = "Хотите получить топ N вакансий по минимальной зарплате? да/нет: "
 
         assert first_string in captured.out
         assert string_keyword in captured.out
         assert user_keyword_str in captured.out
+        assert string_for_save_vacancies in captured.out
+        assert next_step_string in captured.out
 
     with patch("builtins.input", side_effect=["2", "100000", "5"]):
         user_interface()
@@ -27,11 +32,15 @@ def test_user_interface_output(capsys, first_string) -> None:
         string_min_salary_str = f"Вы ввели минимальную зарплату 100000"
         string_n_for_top_str = f"Вы ввели число N = 5"
 
+        string_save_vacancies_info = (f'Топ 5 вакансий по минимальной зарплате 100000 руб. '
+                               f'сохранены по пути "../data/top_5_salary_100000.json"')
+
 
         assert first_string in captured.out
         assert string_min_salary in captured.out
         assert string_min_salary_str in captured.out
         assert string_n_for_top_str in captured.out
+        assert  string_save_vacancies_info in captured.out
 
 
 def test_invalid_input() -> None:
@@ -53,3 +62,16 @@ def test_invalid_input() -> None:
             user_interface()
 
     assert str(exc_info.value) == 'Необходимо ввести целое число'
+
+
+def test_case_independence(capsys) -> None:
+    """Проверяем регистронезависимость ключевого слова"""
+    with patch("builtins.input", side_effect=["1", "Python", "Нет"]):
+        user_interface()
+
+        str_for_compare = "Python"
+        str_for_compare.lower()
+
+        captured = capsys.readouterr()
+
+        assert 'Вы ввели слово "python"' in captured.out
