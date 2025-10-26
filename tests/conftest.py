ef737362ce8b71ddd60e3bb3585442a8d1f56feb
Aksenov Aleksandr <sandroaksyonov@gmail.com>
2025-10-26 06:25:28 +0300

    Create conftest one level higher

--- tests/conftest.py
+++ tests/conftest.py
@@ -81,3 +81,25 @@ def vacancies_list() -> Dict[str, List[Dict[str, Any]]]:
         "suggests": None,
         "alternate_url": "https://hh.ru/search/vacancy?enable_snippets=true"
     })
+
+
+@pytest.fixture
+def vacancy_data1() -> Dict[str, str | Dict]:
+    """Фикстура с данными о вакансии"""
+    return {
+        "name": "Python Developer",
+        "alternate_url": "https://hh.ru/vacancy/123456",
+        "salary": {"from": 350000, "to": 450000, "currency": "RUB", "gross": False},
+        "requirement": "Требования: опыт работы от 3 лет..."
+    }
+
+
+@pytest.fixture
+def vacancy_data2() -> Dict[str, str | Dict]:
+    """Фикстура с данными о вакансии"""
+    return {
+        "name": "Web Developer",
+        "alternate_url": "https://hh.ru/vacancy/123457",
+        "salary": {"from": 250000, "to": 400000, "currency": "RUB", "gross": False},
+        "requirement": "Требования: опыт работы от 5 лет..."
+    }
