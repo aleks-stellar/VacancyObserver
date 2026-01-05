6db868b277bf5c70aaaa2a64f114c5eb84751b62
Aksenov Aleksandr <sandroaksyonov@gmail.com>
2026-01-05 23:23:05 +0300

    initialisation project_3 commit

---
+++ src/data_base/manager.py
@@ -0,0 +1,20 @@
+import psycopg2
+
+
+class DBManager:
+    pass
+
+    def get_companies_and_vacancies_count(self):
+        pass
+
+    def get_all_vacancies(self):
+        pass
+
+    def get_avg_salary(self):
+        pass
+
+    def get_vacancies_with_higher_salary(self):
+        pass
+
+    def get_vacancies_with_keyword(self):
+        pass
